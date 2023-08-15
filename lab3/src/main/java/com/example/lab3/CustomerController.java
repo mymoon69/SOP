1bb5788fd2335a4cee989edde0c7465611f0b4a2
mymoon69 <88487704+mymoon69@users.noreply.github.com>
2023-08-15 12:02:54 +0700

    Update CustomerController.java

--- lab3/src/main/java/com/example/lab3/CustomerController.java
+++ lab3/src/main/java/com/example/lab3/CustomerController.java
@@ -1,8 +1,25 @@
 package com.example.lab3;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CustomerController {
+    private List<Customer> customers;
+
+    public CustomerController() {
+        this.customers = new ArrayList<>();
+        this.customers.add(new Customer("1010","John","Male",25));
+        this.customers.add(new Customer("1018","Peter","Male",24));
+        this.customers.add(new Customer("1019","Sara","Female",23));
+        this.customers.add(new Customer("1110","Rose","Female",23));
+        this.customers.add(new Customer("1001","Emma","Female",30));
+    }
+
+//    public List<Customer> getCustomers()
 
 }
